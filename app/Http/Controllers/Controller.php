11c86303569f371466f2ca22f9c7f6a658a0626e
thiago.henrique <theago.dev@gmail.com>
2025-04-06 19:42:03 -0300

    👨‍🔧 ops: setting up code sniffers PS2 and PSR12.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function teste($teste)
+    {
+        return true;
+    }
 }
